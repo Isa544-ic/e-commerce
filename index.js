e9c660c96f36b41e33408aebe7dbd16d0e62163c
Isa544-ic <isabellacrivelari20@gmail.com>
2025-05-19 17:24:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function mostrarMenu() {
+    document.getElementById("menu").classList.toggle("show"); // abre e fecha o menu hamburguer
+  }
+
+  function sapato() {
+    window.location.href = "bolo.html"; // Redireciona para a página do produto sapato
+  }
+
+  function camisa() {
+    window.location.href = "festa.html"; // Redireciona para a página do produto camisa
+  }
+
+  function calca() {
+    window.location.href = "docinhos.html"; // Redireciona para a página do produto calça
+  }
